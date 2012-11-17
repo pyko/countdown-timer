fdc3fe675b648420cac2a89e4f0587d7aa769817
Peggy <psychopyko@gmail.com>
2012-11-17 22:44:25 +1100

    Moving example files into example dir

--- example/main.js
+++ example/main.js
@@ -1,7 +1,9 @@
+//  Example of how to use the timer
 document.addEventListener('DOMContentLoaded', function(){
 	var countdownHolder = document.querySelector('#countdown-holder');
 
 	countdownHolder.addEventListener('click', function(){
+		// 5 minute timer, counting down every 15 seconds until the last 30 seconds
 		SimpleCountdown.startTimer(this, 300, 15, 30);
 	});
 });
